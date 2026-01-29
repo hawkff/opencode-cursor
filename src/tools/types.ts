57e88bf34d13307f8a5a35d1c7bfafbf6ced1673
Nomadcxx <noovie@gmail.com>
2026-01-29 14:33:19 +1100

    feat: add tool registry for OpenCode tools

---
+++ src/tools/types.ts
@@ -0,0 +1,29 @@
+export interface ToolDefinition {
+  type: "function";
+  function: {
+    name: string;
+    description: string;
+    parameters: {
+      type: "object";
+      properties: Record<string, any>;
+      required?: string[];
+    };
+  };
+}
+
+export interface ToolCall {
+  id: string;
+  type: "function";
+  function: {
+    name: string;
+    arguments: string;
+  };
+}
+
+export interface ToolResult {
+  tool_call_id: string;
+  role: "tool";
+  content: string;
+}
+
+export type ToolExecutor = (args: any) => Promise<string>;
